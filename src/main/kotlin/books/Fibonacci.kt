1d0a4e0373c118cde6797bbd3a45b45865a2222b
cocodori <moradulee@gmail.com>
2022-08-24 14:28:46 +0900

    피보나치

---
+++ src/main/kotlin/books/Fibonacci.kt
@@ -0,0 +1,25 @@
+package books
+
+fun fibonacci(k: Int): Int {
+    if (k <= 1) {
+        return k
+    }
+
+    var first = 1
+    var second = 0
+    var result = 0
+
+    for (i in 0 until k) {
+        result = first + second
+        second = first
+        first = result
+    }
+
+    return result
+}
+
+fun main() {
+    for (i in 0..10) {
+        println(fibonacci(i))
+    }
+}
