dfedbcb0c6bbc40fc1dbb36eb7ce99ca546f8b5e
cocodori <moradulee@gmail.com>
2023-01-21 22:36:04 +0900

    dispatcher servlet으로 변경

--- helloboot/src/main/java/tobyspring/helloboot/HelloController.java
+++ helloboot/src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,9 +1,12 @@
 package tobyspring.helloboot;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Objects;
 
+@RequestMapping("/hello")
 public class HelloController {
 
     private final HelloService helloService;
@@ -12,6 +15,8 @@ public class HelloController {
         this.helloService = helloService;
     }
 
+    @GetMapping
+    @ResponseBody
     public String hello(final String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
