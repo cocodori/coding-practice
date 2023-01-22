7c5db7fe875ed33ecceaa5e357dc96d3ddd2b969
cocodori <moradulee@gmail.com>
2023-01-22 19:20:57 +0900

    Configuration -> ComponentScan

--- helloboot/src/main/java/tobyspring/helloboot/HelloController.java
+++ helloboot/src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,11 +1,15 @@
 package tobyspring.helloboot;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
+@RestController
 @RequestMapping("/hello")
 public class HelloController {
 
