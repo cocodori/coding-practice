9734d5f2b56ae1ed2db74a536f8787226d3f72a0
cocodori <moradulee@gmail.com>
2023-04-18 22:51:37 +0900

    image banner

--- netflix-clone/src/api/axios.js
+++ netflix-clone/src/api/axios.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const instance = axios.create(
     {
-        baseURL: "https://api.themoivedb.org/3",
+        baseURL: "https://api.themoviedb.org/3",
         params: {
-            api_key: "4!uyP85NXsD.RHq",
+            api_key: "5e30692389be2dc5808c0bde7d1240c2",
             language: "ko-KR",
         },
     }
